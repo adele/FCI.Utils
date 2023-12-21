8a3ebca5f7a521e2b2c6d333aa5592cf4616ba8f
adele <adele.helena@gmail.com>
2023-12-21 19:31:48 +0100

    Fixing example with mixedCITests

--- Examples/example_subsets_mixedcitests.R
+++ Examples/example_subsets_mixedcitests.R
@@ -6,7 +6,7 @@ library(pcalg)
 library(doParallel)
 registerDoParallel(cores=7)
 
-type =  "discr2_nc"
+type =  "discr1_nc"
 adag_out <- getDAG(type)
 truePAG <- getTruePAG(adag_out$dagg)
 trueAdjM <- truePAG@amat
@@ -17,7 +17,7 @@ renderAG(trueAdjM, output_folder, fileid = "truePAG", type = "png",
          add_index = FALSE)
 
 
-N = 10000 # sample size
+N = 100000 # sample size
 type = "binary"  # "continuous"
 
 # Generating the dataset with variables as columns and observations as rows
@@ -27,22 +27,51 @@ head(dat)
 
 alpha = 0.05
 all_vars <- colnames(dat)
-covs_names = c()
 
-citestResults <- runAllMixedCITests(dat, vars_names=all_vars,
-                                    covs_names=covs_names, alpha=alpha)
+###########################
+# Running using binCItest #
+###########################
+
+vars_names = all_vars
+vars_df <- dat[,vars_names, drop=FALSE]
+
+indepTest2 <- binCItest
+suffStat2 <- list(dm=dat, adaptDF=TRUE)
+citestResults2 <- runAllCITests(dat, indepTest2, suffStat2, alpha=alpha)
+
+fci_out2 <- runFCIHelper(indepTest, suffStat, alpha=alpha,
+                         labels=vars_names, fileid=fileid,
+                         output_folder=output_folder)
+
+##############################
+# Running using mixedCITests #
+##############################
+
+vars_names = all_vars
+vars_df <- dat[,vars_names, drop=FALSE]
+covs_names = c()
 
 indepTest <- mixedCITest
 suffStat <- getMixedCISuffStat(dat = dat,
-                               vars_names = all_vars,
+                               vars_names = vars_names,
                                covs_names = covs_names)
+citestResults <- runAllCITests(vars_df, indepTest, suffStat, alpha=alpha)
 suffStat$citestResults <- citestResults
 
+fileid <- paste0(vars_names, collapse="_")
+fci_out <- runFCIHelper(indepTest, suffStat, alpha=alpha,
+                        labels=vars_names, fileid=fileid,
+                        output_folder=output_folder)
+
+
+############################
+# Running over all subsets #
+############################
 
 edgeTypesList <- NULL
 metrics <- data.frame()
-for (n in 3:length(all_vars)) {
-  subsets <- combn(all_vars, n)
+for (n in 3:length(vars_names)) {
+  subsets <- combn(vars_names, n)
   for (j in 1:ncol(subsets)) {
     cur_var_names <- subsets[,j]
     cur_dat <- dat[,cur_var_names]
@@ -51,9 +80,9 @@ for (n in 3:length(all_vars)) {
                                    vars_names = cur_var_names,
                                    covs_names = covs_names)
     suffStat$citestResults <- extractValidCITestResults(citestResults,
-                                                        all_vars, cur_var_names)
+                                                        vars_names, cur_var_names)
 
-    fileid <- paste0(labels, collapse="_")
+    fileid <- paste0(cur_var_names, collapse="_")
     fci_out <- runFCIHelper(indepTest, suffStat, alpha=alpha,
                             labels=cur_var_names, fileid=fileid,
                             output_folder=output_folder)
